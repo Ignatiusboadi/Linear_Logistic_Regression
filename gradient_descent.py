b5a854f6cdbda45f23308375320b9dd45417e244
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-03-12 15:40:22 +0000

    added the initialization method for the GradientDescent class; modified docstring of same class

--- gradient_descent.py
+++ gradient_descent.py
@@ -9,13 +9,29 @@ class GradientDescent:
 
     Attributes
     ----------
-    lr: float
-        outputs the learning rate used (to be used) in the algorithm
-    beta: float
-        outputs the value for beta (momentum) used.
-    batch: int
-        number of datasets to be used for each iteration in training the model. An output of None indicates batch
-        gradient descent was used; 1 indicates stochastic gradient descent algorithm was implemented
     epochs: int
         outputs the number of epochs set to run in the implementation of the gradient descent algorithm.
+    lr: float
+        outputs the learning rate used (to be used) in the algorithm.
+    batch: int
+        number of datasets to be used for each iteration in training the model. An output of None indicates batch
+        gradient descent was used; 1 indicates stochastic gradient descent algorithm was implemented.
+    beta: float
+        outputs the value for beta (momentum) used.
+    loss_function: function
+        outputs function used as loss function in the implementation of the algorithm.
+    grad_function: function
+        outputs the gradient of the loss function.
+
     """
+
+    def __init__(self, x, y, epochs, lr, batch, beta, loss_function, grad_function):
+        self.x = x
+        self.y = y
+        self.epochs = epochs
+        self.lr = lr
+        self.batch = batch
+        self.beta = beta
+        self.loss_function = loss_function
+        self.grad_function = grad_function
+        self.weights = None
