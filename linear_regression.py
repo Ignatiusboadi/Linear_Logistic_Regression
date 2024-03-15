173f478d12d5d570c05a3373e901cb28998d64d6
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-03-15 18:36:39 +0000

    added docstring to class; added function to compute r-square for given features and target matrices.

--- linear_regression.py
+++ linear_regression.py
@@ -5,20 +5,50 @@ from gradient_descent import GradientDescent
 
 class LinearRegression:
     """
-    This class implements linear regression for given input matrix and output vector (matrix).
+    This class implements linear regression for given input matrix and output vector (matrix). The user can
+    specify which kind of gradient descent algorithm they want to use for learning the weights, the number of epochs,
+    the learning rate for the algorithm and beta, the momentum rate if any.
     Attributes
     -----------
-
+    x: np.ndarray
+        outputs the features matrix to be used in training the linear regression model.
+    y: np.ndarray
+        outputs the target vector(matrix) to be used in training the linear regression model.
+    epochs: int
+        outputs the number of epochs set to run in the implementation of the gradient descent algorithm.
+    lr: float
+        outputs the learning rate used (to be used) in the algorithm.
+    batch: int
+        number of datasets to be used for each iteration in training the model. An output of None indicates batch
+        gradient descent was used; 1 indicates stochastic gradient descent algorithm was implemented.
+    beta: float
+        outputs the value for beta (momentum) used.
+    weights: np.ndarray
+        outputs the weights determined from the implementation of the gradient descent algorithm.
+    losses: np.ndarray
+        outputs the average loss for each epoch from the implementation of the gradient descent algorithm.
 
     Methods
     --------
-
+    make_prediction:
+        makes prediction using a given features matrix and the weights determined from the Gradient descent
+        algorithm.
+    fit:
+        implements the gradient descent algorithm using the given features matrix and target vector to find
+        the weights.
+    loss_function:
+        computes the loss using given weights, a feature matrix and a target vector.
+    grad_function:
+        computes the gradient of the loss using given weights, a feature matrix and a target vector.
+    r_square:
+        computes the r-square on the training set if no data is passed. When a dataset is passed,
+        makes a prediction using x and the weights, then computes the loss using the values of y passed.
     """
 
     def __init__(self, x, y, epochs=1000, lr=0.1, batch=None, beta=0.0):
         """
-        Initialize regression model for given input matrix X and target matrix y.
-        :param X: np.ndarray
+        Initialize regression model for a given input matrix X and target matrix y.
+        :param x: np.ndarray
             Input matrix X
         :param y: np.ndarray
             target matrix y
@@ -40,8 +70,17 @@ class LinearRegression:
         self.lr = lr
         self.batch = self.x.shape[0] if batch is None else batch
         self.weights = None
+        self.losses = None
 
     def make_prediction(self, x):
+        """
+        makes prediction using a given features matrix and the weights determined from the Gradient descent
+        algorithm.
+        :param x: np.ndarray
+            a features matrix.
+        :return: np.ndarray
+            predicted values using the matrix and computed weights.
+        """
         return x @ self.weights
 
     def grad_function(self, x, y, weights):
@@ -59,3 +98,8 @@ class LinearRegression:
         grad_descent.fit()
         self.weights = grad_descent.weights
         self.losses = grad_descent.losses
+
+    def r_square(self, x=None, y=None):
+        x = self.x if x is not None else x
+        y = self.y if y is not None else y
+        return self.loss_function(x, y, self.weights)
