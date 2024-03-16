f58a6bc1820df54791c1bc54724b954323311ca0
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-03-16 00:46:06 +0000

    added attributes to docstring of logisticregression class; changed name of attribute `n_epochs` to `epochs` for consistency across the files

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,11 +6,37 @@ import gradient_descent
 
 class LogisticRegression:
     """
+    A simple implementation of logistic regression using numpy.
+
+    This class provides functionality for training a logistic regression model
+    on labeled binary classification data using gradient descent optimization.
+    It supports both batch gradient descent and stochastic gradient descent.
+
+    Attributes
+    ----------
+    x: np.ndarray
+        outputs the features matrix to be used in training the linear regression model.
+    y: np.ndarray
+        outputs the target vector(matrix) to be used in training the linear regression model.
+    epochs: int
+        outputs the number of epochs set to run in the implementation of the gradient descent algorithm.
+    lr: float
+        outputs the learning rate used (to be used) in the algorithm.
+    batch: int
+        number of datasets to be used for each iteration in training the model. An output of None indicates batch
+        gradient descent was used; 1 indicates stochastic gradient descent algorithm was implemented.
+    beta: float
+        outputs the value for beta (momentum) used.
+    weights: np.ndarray
+        outputs the weights determined from the implementation of the gradient descent algorithm.
+    losses: np.ndarray
+        outputs the average loss for each epoch from the implementation of the gradient descent algorithm.
 
     """
+
     def __init__(self, x, y, epochs=100, lr=0.1, beta=0, batch=None):
         """
-
+        Initialize regression model for a given input matrix X and target matrix y.
         :param x: np.ndarray
             input matrix
         :param y: np.ndarray
@@ -26,11 +52,11 @@ class LogisticRegression:
         """
         self.x = x
         self.y = y
-        self.n_epochs = epochs
+        self.epochs = epochs
         self.lr = lr
         self.beta = beta
         self.batch = self.x.shape[0] if batch is None else batch
-        self.train_losses = []
+        self.losses = []
         self.weights = None
 
     def add_ones(self, x):
@@ -62,7 +88,9 @@ class LogisticRegression:
         return -1 / x.shape[0] * x.T @ (y - y_pred)
 
     def fit(self):
-        grad_descent = gradient_descent.GradientDescent(self.add_ones(self.x), self.y, epochs=100, lr=0.1, beta=0,
-                                                    batch=None, loss_function=self.cross_entropy,
-                                                    grad_function=self.grad_function)
+        grad_descent = gradient_descent.GradientDescent(self.add_ones(self.x), self.y, self.epochs, lr=0.1, beta=0,
+                                                        batch=None, loss_function=self.cross_entropy,
+                                                        grad_function=self.grad_function)
         grad_descent.fit()
+        self.weights = grad_descent.weights
+        self.losses = grad_descent.losses
